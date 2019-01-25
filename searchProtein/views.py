6e04b48037af7f5648f522f15f7649f05218e64f
diegoalonso78 <diego.alonso@usal.es>
2019-01-25 12:23:54 +0100

    Playing

--- searchProtein/views.py
+++ searchProtein/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
+from .models import Interaction
+from django.template import loader
+
 
 def index(request):
-	latest_interaction_list = Interaction.objects.all()[:5]
-    output = ', '.join([i.participant1 for i in latest_interaction_list])
-   return HttpResponse(output)
+    latest_interaction_list = Interaction.objects.all()[:50]
+    template = loader.get_template('searchProtein/index.html')
+    context = {
+        'interactions' : latest_interaction_list,
+    }
+    
+    return HttpResponse(template.render(context,request))
 
 
-def detail(request,interaction_id):
-	return HttpResponse("You're looking at interaction %s." % interaction_id)
+def detail(request, interaction_id):
+    return HttpResponse("You're looking at interaction %s." % interaction_id)
