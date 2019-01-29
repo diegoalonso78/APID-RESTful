def73486a79573817aaaac9c4363537d246caa11
diegoalonso78 <diego.alonso@usal.es>
2019-01-29 16:53:16 +0100

    Domina el Q en 1 minuto

--- interactions/urls.py
+++ interactions/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 app_name = 'interactions'
 urlpatterns = [
-    path('<slug:protein_id>/', views.index, name='index'),
+    path('<slug:protein_id>/', views.oneProtein, name='oneProtein'),
+    path('<slug:participant1_id>/<slug:participant2_id>/', views.twoProteins, name='twoProteins'),
 ]
--- interactions/views.py
+++ interactions/views.py
@@ -6,11 +6,18 @@ from .serializers import InteractionsJSONSerializer
 from .models import Interaction
 
 
-def index(request, protein_id):
+def oneProtein(request, protein_id):
     serializer = InteractionsJSONSerializer()
-    #query = Protein.objects.filter(Q(uniprot_id__icontains=protein_id) | Q(gene_id__icontains=protein_id) | Q(uniprot_name__icontains=protein_id))
-    #proteins = serializer.serialize(query)
+    
     query = Interaction.objects.filter(Q(participant1__icontains=protein_id) | Q(participant2__icontains=protein_id))
     interactions = serializer.serialize(query)
 
     return JsonResponse(interactions, safe=False)
+
+def twoProteins(request, participant1_id, participant2_id):
+    serializer = InteractionsJSONSerializer()
+    
+    query = Interaction.objects.filter(Q(Q(participant1=participant1_id) & Q(participant2=participant2_id)) | Q(Q(participant1=participant2_id) & Q(participant2=participant1_id)))
+    interactions = serializer.serialize(query)
+
+    return JsonResponse(interactions, safe=False)    
